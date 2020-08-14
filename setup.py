97d4cd6958153bcac20840d25249b29370968417
Tony Ward <tony@statcore.co.uk>
2020-08-14 19:50:48 +0000

    Adjust local package setup to reflect new name

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 
 from setuptools import setup
 
-setup(name='the-dogs',
+setup(name='dogs',
       version='0.1',
       # list folders, not files
-      packages=['the-dogs',
-                'the-dogs.tests'],
-      scripts=['the-dogs/bin/basic_script.py'],
+      packages=['dogs',
+                'dogs.tests'],
+      scripts=['dogs/bin/basic_script.py'],
       )
